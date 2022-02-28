e1143e0d3e9855f5d5a65f00493374b8f0714bdb
Bihan Viranga <bihanviranga@gmail.com>
2022-02-28 16:28:27 +0530

    vim: Add lua LSP config

--- vim/lua/lspconf.lua
+++ vim/lua/lspconf.lua
@@ -42,6 +42,31 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+-- Seperate config for lua
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+nvim_lsp.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = runtime_path
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 -- Auto format using eslint
 vim.cmd([[autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx EslintFixAll]])
 
