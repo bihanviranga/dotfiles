73a90a316e52b373c2d1c1b6c8e68380bd80306f
bihanviranga <bihanviranga@gmail.com>
2023-09-09 00:39:19 +0530

    vim: lua LSP config

--- vim/lua/lspconf.lua
+++ vim/lua/lspconf.lua
@@ -8,6 +8,28 @@ local nvim_lsp = require('lspconfig')
 -- Setup language servers
 nvim_lsp.eslint.setup {}
 nvim_lsp.tsserver.setup {}
+nvim_lsp.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = {
+          'vim',
+          'require',
+          'love',
+        },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 -- Global mappings
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
