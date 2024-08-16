64d0999f9940d69eeed42f01fbd9b618908ffaba
bihanviranga <bihanviranga@gmail.com>
2024-08-16 20:15:40 +0530

    general: sync

--- vim/lazyvim/lua/config/lazy.lua
+++ vim/lazyvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.clangd"},
     -- import/override with your plugins
     { import = "plugins" },
   },
