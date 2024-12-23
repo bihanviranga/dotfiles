1dcd083e1c58e1eadceed9e9e8da63f0f16096f9
bihanviranga <bihanviranga@gmail.com>
2024-12-24 00:03:27 +0530

    vim: blink autocompletion stuff added

---
+++ vim/lazyvim/lua/plugins/blink.lua
@@ -0,0 +1,10 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "super-tab",
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+    },
+  },
+}
