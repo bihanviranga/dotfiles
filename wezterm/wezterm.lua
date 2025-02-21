baf78167fc87752f350d70c7041df50564b335e9
bihanviranga <bihanviranga@gmail.com>
2025-02-22 00:50:36 +0530

    wezterm: initial config added

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.adjust_window_size_when_changing_font_size = false
+
+config.audible_bell = "Disabled"
+
+config.color_scheme = "Catpuccin Mocha"
+
+config.default_prog = { "/opt/homebrew/bin/tmux" }
+
+config.enable_tab_bar = false
+
+return config
