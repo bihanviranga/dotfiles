13062f8880aee1acf56c6de5f63cfe247731175f
bihanviranga <bihanviranga@gmail.com>
2025-04-13 01:54:38 +0530

    emacs: customize the theme

--- emacs/config.el
+++ emacs/config.el
@@ -36,7 +36,11 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-wilmersdorf)
+(setq doom-theme 'doom-plain-dark)
+;; I don't like the default font color -- too unreadable.
+(after! doom-themes
+  (custom-set-faces!
+    '(font-lock-comment-face :foreground "#636163" :slant italic)))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
